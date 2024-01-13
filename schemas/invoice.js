4672c979c52eaf2fdf4719a39f0422fb5ad505b6
mfonisoebong <mfonisoischris@gmail.com>
2024-01-14 00:20:04 +0100

    features: added product update after invoice

--- schemas/invoice.js
+++ schemas/invoice.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { Product } = require("./product");
 
 const InvoiceSchema = new mongoose.Schema(
   {
@@ -25,6 +26,52 @@ const InvoiceSchema = new mongoose.Schema(
     },
   },
 );
+
+InvoiceSchema.post("save", async function (doc) {
+  try {
+    if (doc.status !== "pending") {
+      await updateCartItems(doc.cartItems, doc.status);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+async function updateCartItems(cartItems, status) {
+  return Promise.all(
+    cartItems.map(async (item) => {
+      const product = await Product.findById(item.product._id);
+
+      if (!product) return;
+
+      const productVariants = product.variants.map((v) => {
+        if (v.unlimited) return v;
+
+        return {
+          ...v,
+          quantity:
+            v._id.toString() === item.variant
+              ? actionForItem(status, v.quantity, item.quantity)
+              : v.quantity,
+        };
+      });
+
+      product.variants = productVariants;
+
+      await product.save();
+    }),
+  );
+}
+
+function actionForItem(status, total, bought) {
+  if (status === "paid") {
+    return total - bought;
+  }
+  if (status === "cancelled") {
+    return total + bought;
+  }
+  return total;
+}
 
 const Invoice = mongoose.model("Invoice", InvoiceSchema);
 
